96830288aa94b0d6fb36fb5f271aaee91fcf2d88
App Academy Student <student@appacademy.io>
2014-08-26 12:24:16 -0400

    Add model methods for auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe User, :type => :model do
     end
 
     it 'requires a unique email' do
-      FactoryGirl.build(:user, email: 'me@example.com')
+      FactoryGirl.create(:user, email: 'me@example.com')
 
       expect(
         FactoryGirl.build(:user, email: 'me@example.com')
