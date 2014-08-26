f8d3f9fa2126443bf52b9831187648ed267f1e38
Stefan Huynh <huynh.stefan@gmail.com>
2014-08-26 15:07:08 -0400

    Complete auth with users and session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      flash[:errors] = [
+        "The username and password you entered did not match
+        our records. Please double-check and try again."
+      ]
+
+      render :new
+    else
+      sign_in!(user)
+      # redirect_to ...
+    end
+  end
+
+  def destroy
+    sign_out!
+    redirect_to new_session_url
+  end
+
+end
