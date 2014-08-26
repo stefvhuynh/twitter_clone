1f659228672dbd307dc30bd3a9214271f2cc1bd4
Stefan Huynh <huynh.stefan@gmail.com>
2014-08-26 12:24:16 -0400

    Add model methods for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,12 @@ class User < ActiveRecord::Base
   )
 
   def self.find_by_credentials(email, password)
-
+    user = User.find_by_email(email)
+    (user && user.is_password?(password)) ? user : nil
   end
+
+  attr_reader :password
+  after_initialize :ensure_session_token
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
@@ -17,6 +21,18 @@ class User < ActiveRecord::Base
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 
 end
