630ba6a2bb56377c41ad1f8ee3ad04f65531fe96
Stefan Huynh <huynh.stefan@gmail.com>
2014-08-26 22:06:47 -0400

    Allow unfollowing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,13 +40,26 @@ class UsersController < ApplicationController
   end
   
   def follow
-    follow = Follow.new({
+    follow = Follow.new(
       followed_id: params[:id],
       follower_id: current_user.id
-    })
+    )
         
     unless follow.save
       flash[:errors] = follow.errors.full_messages 
+    end
+    
+    redirect_to user_url(params[:id])
+  end
+  
+  def unfollow
+    follow = Follow.where(
+      followed_id: params[:id], 
+      follower_id: current_user.id 
+    ).first
+    
+    unless follow.destroy
+      flash[:errors] = follow.errors.full_messages
     end
     
     redirect_to user_url(params[:id])
