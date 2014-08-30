28a5b01ccbfdb56f85f73a30fea52c98f07b97ed
Stefan Huynh <huynh.stefan@gmail.com>
2014-08-30 13:13:48 -0400

    Move default picture to public

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,15 @@ class PagesController < ApplicationController
   before_filter :require_signed_in!
 
   def home
-    user_tweets = current_user.tweets
-      .includes(:mentioned_users)
-      .includes(:mentioned_hashtags)
+    user_tweets = current_user.tweets.includes(
+      :mentioned_users, 
+      :mentioned_hashtags
+    )
       
-    followed_tweets = current_user.followed_tweets
-      .includes(:mentioned_users)
-      .includes(:mentioned_hashtags)
+    followed_tweets = current_user.followed_tweets.includes(
+      :mentioned_users, 
+      :mentioned_hashtags
+    )
     
     @tweets = user_tweets + followed_tweets
     @tweets.sort_by! { |tweet| Time.now - tweet.created_at }
