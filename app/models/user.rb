61c3bc3fa79b1a608064b3facc38425a9ea10c53
Stefan Huynh <huynh.stefan@gmail.com>
2014-08-30 13:04:44 -0400

    Refactor paperclip default image

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   has_many :followers, through: :follower_follows, source: :follower
   has_many :followed_tweets, through: :followeds, source: :tweets
 
-  has_attached_file :avatar
+  has_attached_file :avatar, default_url: '/images/default_avatar.png'
   validates_attachment_content_type(
     :avatar,
     content_type: /\Aimage\/.*\Z/
